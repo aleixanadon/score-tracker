e13436c5673b9787af79be80b2d621efe0ab109f
aleixanadon <aleixanadon25@gmail.com>
2024-04-20 18:52:48 +0200

    feature: react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage.tsx'
+import ScoreTrackerPage from './pages/ScoreTrackerPage/ScoreTrackerPage.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/score-tracker/",
+    element: <App />,
+    children: [
+      {
+        path: "/score-tracker/",
+        element: <ScoreTrackerPage />
+      },
+      {
+        path: "/score-tracker/home",
+        element: <HomePage />
+      },
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/ScoreTrackerPage/ScoreTrackerPage.tsx
@@ -0,0 +1,8 @@
+
+const ScoreTrackerPage = () => {
+  return (
+    <div>ScoreTrackerPage</div>
+  )
+}
+
+export default ScoreTrackerPage
